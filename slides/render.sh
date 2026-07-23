5d185161d5368fe0f8f24fbdcee2da49d892d651
kevingoneill <oneillkeving@gmail.com>
2026-07-23 08:19:58 -0300

    minor updates

--- slides/render.sh
+++ slides/render.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-decktape -s 1050x700 -p 100 --fragments --chrome-arg=allow-file-access-from-files reveal slides.html A_measure_of_metacognitive_bias_you_can_be_confident_in.pdf
+decktape -s 1050x700 -p 100 --fragments --chrome-arg=allow-file-access-from-files reveal ../_site/slides/index.html A_measure_of_metacognitive_bias_you_can_be_confident_in.pdf
